e43f687483d68ff18c9366b11f4271c8629fabf4
aalenoza <anzamora@up.edu.ph>
2024-06-04 07:19:58 +0800

    feat: profile not working yet

--- lib/pages/organization_profile_page.dart
+++ lib/pages/organization_profile_page.dart
@@ -1,65 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:montero_cmsc23/providers/auth_provider.dart';
+import 'package:montero_cmsc23/providers/credential_provider.dart';
+import 'package:provider/provider.dart';
 
 class OrganizationProfilePage extends StatefulWidget {
   @override
-  _OrganizationProfilePageState createState() => _OrganizationProfilePageState();
+  _OrganizationProfilePageState createState() =>
+      _OrganizationProfilePageState();
 }
 
 class _OrganizationProfilePageState extends State<OrganizationProfilePage> {
-  late User? _currentUser;
-  Map<String, dynamic>? _organizationData;
-
-  @override
-  void initState() {
-    super.initState();
-    _currentUser = FirebaseAuth.instance.currentUser;
-    if (_currentUser != null) {
-      _fetchOrganizationData();
-    }
-  }
-
-  Future<void> _fetchOrganizationData() async {
-    try {
-      DocumentSnapshot<Map<String, dynamic>> snapshot = await FirebaseFirestore.instance
-          .collection('organizations')
-          .doc(_currentUser!.uid)
-          .get();
-
-      if (snapshot.exists) {
-        setState(() {
-          _organizationData = snapshot.data();
-        });
-      }
-    } catch (error) {
-      print("Error fetching organization data: $error");
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
+    String? userID = context.watch<MyAuthProvider>().userID;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Organization Profile'),
-      ),
-      body: _organizationData != null
-          ? Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                ListTile(
-                  title: Text(
-                    'Name: ${_organizationData!['name']}',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  subtitle: Text('Description: ${_organizationData!['description']}'),
-                ),
-                // Add more ListTile widgets to display additional organization information
-              ],
-            )
-          : Center(
-              child: CircularProgressIndicator(),
-            ),
-    );
+        appBar: AppBar(
+          title: const Text('Organization Profile'),
+        ),
+        body: FutureBuilder(
+            future: context.read<CredProvider>().getUserByID(userID),
+            builder: (context, snapshot) {
+              return snapshot.data != null
+                  ? Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ListTile(
+                          title: Text(
+                            'Name: ${snapshot.data!['firstName']} ${snapshot.data!["lastName"]}',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          subtitle:
+                              Text('Description: ${snapshot.data!['email']}'),
+                        ),
+                        // Add more ListTile widgets to display additional organization information
+                      ],
+                    )
+                  : const Center(
+                      child: CircularProgressIndicator(),
+                    );
+            }));
   }
 }
